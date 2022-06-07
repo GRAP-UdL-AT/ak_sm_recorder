06a5f78f5540eb457ed0c5081bbc9310302d2492
Juan Carlos <43625026+juancarlosmiranda@users.noreply.github.com>
2022-06-07 15:18:35 +0200

    Fix documentation
    Fix documentation

--- ak_sm_recorder_start.bat
+++ ak_sm_recorder_start.bat
@@ -1,9 +1,6 @@
 @echo off
 
-set VIRTUAL_ENV=%HOMEDRIVE%%HOMEPATH%\development_env\ak_sm_recorder_venv
-
 rem This file is UTF-8 encoded, so we need to update the current code page while executing it
-
 for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
     set _OLD_CODEPAGE=%%a
 )
@@ -11,13 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
+set VIRTUAL_ENV=%HOMEDRIVE%%HOMEPATH%\development_env\ak_sm_recorder_venv
+
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venv) %PROMPT%
+set PROMPT=(ak_sm_recorder_venv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
